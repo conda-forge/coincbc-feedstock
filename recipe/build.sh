f1ca8f225b8c67056fbc925c821137aec081e622
Anthony Scopatz <scopatz@gmail.com>
2020-02-05 10:49:23 -0600

    set FLIBS

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,10 @@ export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
 
+if [ "${UNAME}" == "Linux" ]; then
+  export FLIBS="-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm"
+fi
+
 # Use only 1 thread with OpenBLAS to avoid timeouts on CIs.
 # This should have no other affect on the build. A user
 # should still be able to set this (or not) to a different
